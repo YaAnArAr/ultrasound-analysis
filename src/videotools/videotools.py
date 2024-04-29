a29baa35e93e7def5f485fa22a74bc0ec0c56c25
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2024-04-29 22:30:44 +0300

    updated videotools.py

--- src/videotools/videotools.py
+++ src/videotools/videotools.py
@@ -1,6 +1,6 @@
-'''
+"""
 This module contains classes and functions for video operations
-'''
+"""
 
 from collections.abc import Generator
 from pathlib import Path
@@ -8,10 +8,29 @@ from pathlib import Path
 from numpy.typing import NDArray
 
 
-def read_video(path: Path | str) -> Generator[NDArray, None, None]:
-    '''
+def crop_frame(frame: NDArray) -> NDArray:
+    """
+    Crop frame
+    :param frame: frame that need to crop
+    :returns: cropped frame
+    """
+    x_first_offset = 150
+    x_second_offset = 100
+    y_offset = 20
+    x, y = frame.shape[0], frame.shape[1]
+    cropped_frame = frame[x_first_offset:x - x_second_offset, y_offset:y - y_offset]
+    return cropped_frame
+
+
+def read_video(path: Path | str) -> NDArray:
+    """
     Read the video frame-by-frame
     :param path: path to the video
-    :returns: generator, yielding RGB video frames
-    '''
-    raise NotImplementedError
+    :returns: generator, yielding RGB cropped video frames
+    """
+    cap = cv2.VideoCapture(path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            frame = crop_frame(frame)
+            yield frame.astype('uint8')
