2eee0fad7bcc4b0d0f31ffee536aa8f8411ee598
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2024-05-07 22:13:16 +0300

    demo

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -2,6 +2,6 @@
 This module contains classes and functions for frame processing
 '''
 
-from .processing import PixelColor, get_frame_colors
+from .processing import PixelColor, get_frame_colors, get_clouds, Cloud
 
-__all__ = ['PixelColor', 'get_frame_colors']
+__all__ = ['PixelColor', 'get_frame_colors', 'get_clouds', 'get_clouds_for_color', 'Cloud']
